02e7887731326e9d6e94405ceb29a3ec9e9f0b05
Munachimso Ukaoha <65025073+Munachidozie@users.noreply.github.com>
2023-01-16 22:04:55 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+print ("-- Comic Book Character Creator --")
+print (" ")
+jump = input ("Are you sarcastic and fun to hang around (literally and metaphorically) ?:  ")
+
+if jump == "yes":
+  print ("Then you're Spiderman")
+elif input ("Are you commanding and what to rule the world by all means?") == "yes":
+  print ("Then your most likely Doctor Doom")
+elif input ("Do you feel 'incredibly' strong?:  ") == "yes":
+  print ("Then you're the Incredible Hulk")
+else:
+  input ("Then tell us who you might be: ")
